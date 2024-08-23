a56e89a8a1b7e13f737336c8b60cf7aa1ab04346
yoshinari0508 <shohei.yoshinari@hutzper.com>
2024-08-23 14:17:37 +0900

    fixed

--- scripts/start_process.ps1
+++ scripts/start_process.ps1
@@ -1,20 +1,32 @@
-# F5キーを送信するPowerShellスクリプト
-Add-Type -TypeDefinition @"
+# Add user32.dll to call the keybd_event function
+Add-Type @"
 using System;
 using System.Runtime.InteropServices;
 
 public class Keyboard {
-    [DllImport("user32.dll", CharSet = CharSet.Auto)]
-    public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
 
     public const int KEYEVENTF_KEYUP = 0x0002;
-    public const byte VK_F5 = 0x74;
+    public const int VK_CONTROL = 0x11;
+    public const int VK_SHIFT = 0x10;
+    public const int VK_MENU = 0x12; // Alt key
+    public const int VK_F4 = 0x73;
 
-    public static void SendF5() {
-        keybd_event(VK_F5, 0, 0, 0); // F5キーを押す
-        keybd_event(VK_F5, 0, KEYEVENTF_KEYUP, 0); // F5キーを放す
+    public static void SendKey(byte keyCode, bool keyDown) {
+        keybd_event(keyCode, 0, keyDown ? 0 : KEYEVENTF_KEYUP, 0);
     }
 }
 "@
 
-[Keyboard]::SendF5()
+# Send Ctrl + Shift + Alt + F4
+[Keyboard]::SendKey([Keyboard]::VK_CONTROL, $true)
+[Keyboard]::SendKey([Keyboard]::VK_SHIFT, $true)
+[Keyboard]::SendKey([Keyboard]::VK_MENU, $true)
+[Keyboard]::SendKey([Keyboard]::VK_F4, $true)
+
+# Release the keys
+[Keyboard]::SendKey([Keyboard]::VK_F4, $false)
+[Keyboard]::SendKey([Keyboard]::VK_MENU, $false)
+[Keyboard]::SendKey([Keyboard]::VK_SHIFT, $false)
+[Keyboard]::SendKey([Keyboard]::VK_CONTROL, $false)
