e2b4755c50702694f8ba4e8e457a779e5953f671
yoshinari0508 <shohei.yoshinari@hutzper.com>
2024-09-04 11:37:43 +0900

    fixed

--- scripts/start_process2.ps1
+++ scripts/start_process2.ps1
@@ -1,16 +1,9 @@
-# Visual Studioのパスを指定して、ソリューションファイルを開くジョブを開始
-$job = Start-Job -ScriptBlock {
-    $visualStudioPath = "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
-    $solutionPath = "C:\CSharp.Hutzper.Library\Hutzper.Library.sln"
-    Start-Process $visualStudioPath -ArgumentList $solutionPath
+# ソリューションファイルのパスを指定してプロセスを開始
+Start-Process "C:\CSharp.Hutzper.Library\Hutzper.Library.sln"
 
-    # 30秒間待機
-    Start-Sleep -Seconds 30
+# 30秒間待機
+Start-Sleep -Seconds 30
 
-    # F5キーを送信して実行
-    Add-Type -AssemblyName System.Windows.Forms
-    [System.Windows.Forms.SendKeys]::SendWait("{F5}")
-}
-
-# ジョブ情報を出力
-$job
+# F5キーを送信して実行
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.SendKeys]::SendWait("{F5}")
