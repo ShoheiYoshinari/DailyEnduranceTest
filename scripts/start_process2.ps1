81fe6b4e27bc322cb40a0b67e5204e4a348cf8fe
yoshinari0508 <shohei.yoshinari@hutzper.com>
2024-09-04 10:19:35 +0900

    fixed

--- scripts/start_process2.ps1
+++ scripts/start_process2.ps1
@@ -1,5 +1,8 @@
-# ソリューションファイルをバックグラウンドで起動する
-Start-Process "C:\CSharp.Hutzper.Library\Hutzper.Library.sln" -NoNewWindow
+# Visual Studioのパスを指定して、ソリューションファイルを開く
+$visualStudioPath = "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+$solutionPath = "C:\CSharp.Hutzper.Library\Hutzper.Library.sln"
+
+Start-Process $visualStudioPath -ArgumentList $solutionPath -NoNewWindow -PassThru
 
 # 30秒間待機
 Start-Sleep -Seconds 30
