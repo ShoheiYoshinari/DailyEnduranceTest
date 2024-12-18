b53829389f1ef3e2e10a34e6a0703219f925f1a3
yoshinari0508 <shohei.yoshinari@hutzper.com>
2024-12-18 12:16:50 +0900

    fixed

--- scripts/start_dotnet_trace.ps1
+++ scripts/start_dotnet_trace.ps1
@@ -1,25 +1,29 @@
 param (
-    [Parameter(Mandatory=$true)]
-    [int]$ProcessId,      # プロセス ID
-
-    [Parameter(Mandatory=$true)]
-    [TimeSpan]$Duration,  # トレースの期間 (TimeSpan オブジェクト)
-
-    [Parameter(Mandatory=$true)]
-    [string]$OutputFile   # 出力ファイルのパス
+    [string]$ProcessId,
+    [int]$Duration,
+    [string]$OutputFile
 )
 
-# dotnet-trace のコマンドを作成
-$dotnetTraceCommand = "dotnet trace collect --process-id $ProcessId --duration $($Duration.TotalSeconds) --output $OutputFile"
+# 分を dd:hh:mm:ss に変換
+$days = [math]::Floor($tests.duration_minutes / 1440)
+$hours = [math]::Floor(($tests.duration_minutes % 1440) / 60)
+$minutes = $tests.duration_minutes % 60
+$seconds = 0  # 秒は常に0と仮定
+# 各部分を2桁の文字列にフォーマット
+$daysStr = $days.ToString("00")
+$hoursStr = $hours.ToString("00")
+$minutesStr = $minutes.ToString("00")
+$secondsStr = $seconds.ToString("00")
+$durationStr =  "${daysStr}:${hoursStr}:${minutesStr}:${secondsStr}"
+#  結果を表示
+Write-Host $durationStr
 
-Write-Host "Executing: $dotnetTraceCommand"
+Write-Host "Starting dotnet trace..."
+dotnet trace collect --process-id $ProcessId --profile gc-collect --duration $durationStr --output $OutputFile
 
-# dotnet-trace コマンドを実行
-try {
-    # dotnet-trace コマンドを非同期で実行
-    Invoke-Expression $dotnetTraceCommand
-    Write-Host "dotnet trace completed successfully."
-} catch {
-    Write-Host "Error occurred while executing dotnet trace: $_"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "dotnet trace failed with exit code $LASTEXITCODE"
     exit 1
 }
+
+Write-Host "dotnet trace completed successfully. Output file: $OutputFile"
